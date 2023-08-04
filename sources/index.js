e02c2d86938661fab07405401f5dd268151ba6b4
Jordan Anastassiades <jordan.astass@gmail.com>
2023-08-04 15:29:24 +0200

    MasterCardGenerator

--- sources/index.js
+++ sources/index.js
@@ -1,2 +1,2 @@
-// module.exports.MasterCardGenerator = require('./classes/MasterCardGenerator.js')
+module.exports.MasterCardGenerator = require("./classes/MasterCardGenerator.js");
 // module.exports.SnowflakeUtil = require('./classes/SnowflakeUtil.js')
